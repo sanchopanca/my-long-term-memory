a2f142b5ff8bcb47f0007b1b03e59456ae389bff
Aleksandr Kovalev <aleksandr@kovalev.engineer>
2017-03-04 23:53:14 +0100

    Fix all issues with adding new entry

--- mltm.pm
+++ mltm.pm
@@ -13,9 +13,9 @@ sub add_entry {
     INSERT INTO entries (title, content)
     VALUES (?, ?)
     --
-    $dbh->do($insert_statement, $title, $text);
+    $dbh->do($insert_statement, undef, $title, $text);
     my $entry_id = _get_last_inserted_id('entries');
-    _connect_tags_to_entry($tags_ids, $entry_id);
+    _update_tags_to_entry($tags_ids, $entry_id);
 }
 
 sub get_tag_id {
@@ -23,18 +23,19 @@ sub get_tag_id {
     my $select_statement = <<'    --';
     SELECT rowid
     FROM tags
-    WHERE tag = ?
+    WHERE name = ?
     --
-    my $rows = $dbh->selectall_arrayref($select_statement, $tag);
-    if (@{ $rows } > 0) {
+    my $rows = $dbh->selectall_arrayref($select_statement, undef, $tag);
+    if (scalar @{ $rows } > 0) {
         return $rows->[0][0];
     } else {
         my $insert_statement = <<'        --';
-        INSERT into tags (tag)
+        INSERT into tags (name)
         VALUES (?)
         --
-        $dbh->do($insert_statement, $tag);
-        return _get_last_inserted_id('tags');
+        $dbh->do($insert_statement, undef, $tag);
+        my $id = _get_last_inserted_id('tags');
+        return $id;
     }
 }
 
@@ -55,7 +56,7 @@ sub update_entry {
     WHERE
         rowid = ?
     --
-    $dbh->do($update_statement, $title, $text, $id);
+    $dbh->do($update_statement, undef, $title, $text, $id);
     my $tags_ids = _get_tags_ids($tags);
     _update_tags_to_entry($tags_ids, $id);
 }
@@ -66,26 +67,28 @@ sub _update_tags_to_entry {
     DELETE FROM tags_entries
     WHERE entry_id = ?
     --
-    $dbh->do($delete_statement, $entry_id);
+    $dbh->do($delete_statement, undef, $entry_id);
     for my $tag_id (@{ $tags_ids }) {
         my $insert_statement = <<'        --';
         INSERT INTO tags_entries (tag_id, entry_id)
         VALUES (?, ?)
         --
-        $dbh->do($insert_statement, $tag_id, $entry_id);
+        $dbh->do($insert_statement, undef, $tag_id, $entry_id);
     }
 }
 
 sub _get_last_inserted_id {
     my ($table) = @_;
-    return 0;
+    my $id = $dbh->last_insert_id(undef, undef, $table, 'rowid');
+    return $id;
 }
 
 sub _get_tags_ids {
     my ($tags) = @_;
     my @tags_ids;
     for my $tag (@{ $tags }) {
-        push(@tags_ids, get_tag_id($tag));
+        my $id = get_tag_id($tag);
+        push(@tags_ids, $id);
     }
     return \@tags_ids;
 }
