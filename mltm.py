66bc7235a830fd03d63074c0613647c7b790da10
Aleksandr Kovalev <aleksandr@kovalev.engineer>
2017-03-06 21:00:43 +0100

    Add search by tag_id

--- mltm.py
+++ mltm.py
@@ -1,28 +1,19 @@
-import sqlite3
-
-
-class DB:
-    def __init__(self):
-        self.connection = sqlite3.connect('mltm.db')
-        self.cursor = self.connection.cursor()
-
-    def execute(self, statement, params=()):
-        self.cursor.execute(statement, params)
-        self.connection.commit()
-
-    def execute_and_fetch_one(self, statement, params=()):
-        self.cursor.execute(statement, params)
-        return self.cursor.fetchone()
-
-    def execute_and_fetch_all(self, statement, params=()):
-        self.cursor.execute(statement, params)
-        return self.cursor.fetchall()
-
-    def get_last_rowid(self):
-        return self.cursor.lastrowid
+from database import DB
 
 
 db = DB()
+
+
+class Entry:
+    def __init__(self, id, title, text, tags):
+        self.id = id
+        self.title = title
+        self.text = text
+        self.tags = tags
+
+    def __repr__(self):
+        return 'id: {}, title: {}, text: {}, tags: {}'.\
+            format(self.id, self.title, self.text, self.tags)
 
 
 def add_entry(title, text, tags):
@@ -36,14 +27,35 @@ def add_entry(title, text, tags):
     _update_tags_to_entry(tags_ids, entry_id)
 
 
-def get_tag_id(tag):
+def get_entry_by_id(entry_id):
+    select_statement = '''
+    SELECT entries.title,
+           entries.content
+    FROM entries
+    WHERE rowid = ?
+    '''
+    entry = db.execute_and_fetch_one(select_statement, (entry_id,))
+    if entry is None:
+        return None
+    select_statement = '''
+    SELECT tags.name
+    FROM tags
+    INNER JOIN tags_entries ON tags.rowid = tags_entries.tag_id
+    WHERE tags_entries.entry_id = ?
+    '''
+    tags = _transpose(db.execute_and_fetch_all(select_statement, (entry_id,)))
+    title, text = entry
+    return Entry(entry_id, title, text, tags)
+
+
+def get_tag_id(tag, insert_new=False):
     select_statement = '''
     SELECT rowid
     FROM tags
     WHERE name = ?
     '''
     tag_id = db.execute_and_fetch_one(select_statement, (tag,))
-    if tag_id is None:
+    if tag_id is None and insert_new:
         insert_statement = '''
         INSERT into tags (name)
         VALUES (?)
@@ -54,7 +66,14 @@ def get_tag_id(tag):
 
 
 def search_by_tag_id(tag_id):
-    pass
+    select_statement = '''
+    SELECT entries.rowid
+    FROM entries
+    INNER JOIN tags_entries ON entries.rowid = tags_entries.entry_id
+    WHERE tags_entries.tag_id = ?
+    '''
+    entries_ids = _transpose(db.execute_and_fetch_all(select_statement, (tag_id,)))
+    return [get_entry_by_id(entry_id) for entry_id in entries_ids]
 
 
 def search_arbitrary_text(text):
@@ -89,7 +108,11 @@ def _update_tags_to_entry(tags_ids, entry_id):
 
 
 def _get_tags_ids(tags):
-    return [get_tag_id(tag) for tag in tags]
+    return [get_tag_id(tag, insert_new=True) for tag in tags]
+
+
+def _transpose(x):
+    return list(next(zip(*x), ()))  # Sorry
 
 if __name__ == '__main__':
-    add_entry('test title', 'test txt', ['test1', 'test2'])
+    print(search_by_tag_id(1))
